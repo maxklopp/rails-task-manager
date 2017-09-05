73fab7d44bcb4382bbcb58c50ab27229519eb171
Maximilian Schilling <maximilian.schilling@gmail.com>
2017-09-05 14:44:17 +0200

    Added some methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: 'clean the house', description: 'clean the house fast')
+Task.create(name: 'clean the garden', description: 'clean the garden fast')
